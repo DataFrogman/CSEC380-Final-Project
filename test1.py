54bbef8e30079fb542be0a66e87b4b6424827bb1
Quintin Kovach <datafrogman@outlook.com>
2019-09-16 16:39:25 -0400

    update test1.py

--- test1.py
+++ test1.py
@@ -2,5 +2,5 @@ import pytest
 import requests
 
 def test_mytest():
-    r = requests.get(‘webserver:80’)
-    assert "Hello" in r.text
+    r = requests.get("http://127.0.0.1:80")
+    assert "Hello World" in r.text
