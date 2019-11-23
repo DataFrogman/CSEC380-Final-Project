4dc3d99a8bf451a23d4f46c7cba8c8854c921116
NBrockman <nb3355@rit.edu>
2019-11-23 16:39:15 -0500

    I'm so blind

--- tests/test1.py
+++ tests/test1.py
@@ -7,18 +7,18 @@ import requests
 
 def test_AuthTest():
     data = {'username': 'test', 'password': 'test'}
-    r = requests.get("http://localhost:8080/login", data=data)
+    r = requests.post("http:/127.0.0.1:8080/login", data=data)
     print("Authentication tests----------------")
     assert 'Welcome' in r.text
     print("Correct authentication: passed")
 
     data = {'username': 'testwrong', 'password': 'test'}
-    r = requests.get("http://localhost:8080/login", data=data)
+    r = requests.post("http://127.0.0.1:8080/login", data=data)
     assert 'Invalid' in r.text
     print("Wrong username authentication: passed")
 
     data = {'username': 'test', 'password': 'testwrong'}
-    r = requests.get("http://localhost:8080/login", data=data)
+    r = requests.post("http://127.0.0.1:8080/login", data=data)
     assert 'Invalid' in r.text
     print("Wrong password authentication: passed")
 
