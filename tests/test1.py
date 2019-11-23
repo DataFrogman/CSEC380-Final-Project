8f3a9248b81e4df167450ac8560f3fe79180cc9f
NBrockman <nb3355@rit.edu>
2019-11-23 15:42:41 -0500

    Edited tests

--- tests/test1.py
+++ tests/test1.py
@@ -2,23 +2,23 @@ import pytest
 import requests
 
 def test_mytest():
-    r = requests.get("http://127.0.0.1:8080")
+    r = requests.get("http:/localhost:8080")
     assert "Tiger Advanced" in r.text
 
 def AuthTest():
     data = {'username': 'test', 'password': 'test'}
-    r = requests.get("http://127.0.0.1:8080", data=data)
+    r = requests.get("http://localhost:8080/login", data=data)
     print("Authentication tests----------------")
     assert 'Welcome' in r.text
     print("Correct authentication: passed")
 
     data = {'username': 'testwrong', 'password': 'test'}
-    r = requests.get("http://127.0.0.1:8080", data=data)
+    r = requests.get("http://localhost:8080/login", data=data)
     assert 'Invalid' in r.text
     print("Wrong username authentication: passed")
 
     data = {'username': 'test', 'password': 'testwrong'}
-    r = requests.get("http://127.0.0.1:8080", data=data)
+    r = requests.get("http://localhost:8080/login", data=data)
     assert 'Invalid' in r.text
     print("Wrong password authentication: passed")
 
