a3132511bf622cf3fb5f85a11200a9eb46d8b406
NBrockman <nb3355@rit.edu>
2019-11-23 15:27:01 -0500

    Added Auth Test

--- tests/test1.py
+++ tests/test1.py
@@ -2,5 +2,23 @@ import pytest
 import requests
 
 def test_mytest():
-    r = requests.get("http://127.0.0.1:80")
-    assert "Hello World" in r.text
+    r = requests.get("http://127.0.0.1:8080")
+    assert "Tiger Advanced" in r.text
+
+def AuthTest():
+    data = {'username': 'test', 'password': 'test'}
+    r = requests.get("http://127.0.0.1:8080", data=data)
+    print("Authentication tests----------------")
+    assert 'Welcome' in r.text
+    print("Correct authentication: passed")
+
+    data = {'username': 'testwrong', 'password': 'test'}
+    r = requests.get("http://127.0.0.1:8080", data=data)
+    assert 'Invalid' in r.text
+    print("Wrong username authentication: passed")
+
+     data = {'username': 'test', 'password': 'testwrong'}
+    r = requests.get("http://127.0.0.1:8080", data=data)
+    assert 'Invalid' in r.text
+    print("Wrong password authentication: passed")
+
