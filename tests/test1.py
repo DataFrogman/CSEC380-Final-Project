72b30686f792dcd3d1c84f6cb4de797dc7d0840c
NBrockman <nb3355@rit.edu>
2019-11-23 19:37:53 -0500

    Added a sleep to tests to wait for connection

--- tests/test1.py
+++ tests/test1.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+import time
 
 # def test_mytest():
 #     r = requests.get("http://localhost:8080")
@@ -11,11 +12,13 @@ def test_AuthTest():
     print("Authentication tests----------------")
     assert 'Welcome' in r.text
     print("Correct authentication: passed")
+    time.sleep(0.01)
 
     data = {'username': 'testwrong', 'password': 'test'}
     r = requests.post("http://127.0.0.1:8080/login", data=data)
     assert 'Invalid' in r.content.decode('UTF-8')
     print("Wrong username authentication: passed")
+    time.sleep(0.01)
 
     data = {'username': 'test', 'password': 'testwrong'}
     r = requests.post("http://127.0.0.1:8080/login", data=data)
