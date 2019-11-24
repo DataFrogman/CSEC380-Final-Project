9922a5032667df7aca50965d1fdf025281c99e4b
NBrockman <nb3355@rit.edu>
2019-11-23 22:11:34 -0500

    figuring out delete call

--- tests/test1.py
+++ tests/test1.py
@@ -33,7 +33,8 @@ def test_uploadAndDeleteVid():
     r = sess.post("http://127.0.0.1:8080/manage", files=data)
     r = sess.get("http://127.0.0.1:8080/homepage")
     assert 'Rick_Astley_Never_Gonna_Give_You_Up.mp4' in r.content.decode('UTF-8')
-    r = sess.post("http://127.0.0.1:8080/delete/0")
+    data = { 'videoid': '0'}
+    r = sess.post("http://127.0.0.1:8080/delete", data=data)
     r = sess.get("http://127.0.0.1:8080/homepage")
     assert 'No Videos to Display!' in r.content.decode('UTF-8')
 
